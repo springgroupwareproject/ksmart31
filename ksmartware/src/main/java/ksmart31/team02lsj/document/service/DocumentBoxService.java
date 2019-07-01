213c8f6dd229776d897dfef10a9f5545cd6c8bf9
Administrator <Administrator@MSDN-SPECIAL>
2019-07-01 17:29:00 +0900

    [2019-07-01] 관리자 문서통합관리 - 문서관리 관리자 로그인 시점에서 현재 등록된 모든 문서를 조회. view - C -S - M 작업 완료(리스트 출력)
    사용자
    
    문서함
    기안문서, 결재완료문서, 수신문서, 회람문서, 첨부파일 문서함 리스트 조회가능
    view - C -S - M 작업 완료(리스트 출력)

--- ksmartware/src/main/java/ksmart31/team02lsj/document/service/DocumentBoxService.java
+++ ksmartware/src/main/java/ksmart31/team02lsj/document/service/DocumentBoxService.java
@@ -1,14 +1,11 @@
 package ksmart31.team02lsj.document.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ksmart31.team01.member.domain.Member;
 import ksmart31.team02lsj.document.domain.DocumentAttachedFile;
 import ksmart31.team02lsj.document.domain.DraftDocument;
 import ksmart31.team02lsj.document.mapper.DocumentBoxMapper;
