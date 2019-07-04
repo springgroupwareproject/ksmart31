0296d239748d7ef8a497de6b10e0b3b5adf1e99a
Administrator <Administrator@MSDN-SPECIAL>
2019-07-04 15:41:25 +0900

    [2019-07-04 주진호] 휴가 내역 관리
    - 조직도, 조직원 연결 백엔드 작업 완료

--- ksmartware/src/main/java/ksmart31/team03/leave/service/LeaveHistoryManagementService.java
+++ ksmartware/src/main/java/ksmart31/team03/leave/service/LeaveHistoryManagementService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ksmart31.team01.department.domain.Department;
+import ksmart31.team01.department.domain.DepartmentJoinMember;
 import ksmart31.team03.leave.domain.LeaveCategory;
 import ksmart31.team03.leave.domain.LeaveDetail;
 import ksmart31.team03.leave.domain.LeaveGrant;
@@ -91,9 +92,31 @@ public class LeaveHistoryManagementService {
 		return leaveHistoryByMemberList;
 	}	
 	//조직도 departmentView.html
-	public List<Department> getDepartmentList(){
+	public Map<String, Object> getDepartmentList(String departmentName){
 		//재귀쿼리 사용한 departmentName 조직도 조회 리스트
-		return leaveHistoryMapper.selectDepartmentList();
+		List<Department> recursiveList = leaveHistoryMapper.selectDepartmentList();
+		System.out.println(recursiveList +"DepartmentService getDepartmentList recursiveList");		
+		// StringUtils.countMatches() -> departmentName중에 -가 포함되어있는 갯수를 보여준다
+		int departmentNumber = StringUtils.countMatches(departmentName, "-");
+		System.out.println("DepartmentService getDepartmentList departmentNumber -갯수 : " + departmentNumber);
+		// replaceAll()메서드로 -가 포함된문자열을 없애준다 		
+		if(departmentName != null){	
+			departmentName = departmentName.replaceAll("-","");
+			System.out.println("DepartmentService getDepartmentList departmentName -제거 확인 : " + departmentName);
+		}
+		// 2개 이상의 데이터를 하나로 넘기기 위해 HashMap 사용
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("departmentName", departmentName);
+		map.put("departmentNumber", departmentNumber);		
+		//department부서명  member조직원 기본 정보 조인	
+		List<DepartmentJoinMember> departmentJoinMemberList = leaveHistoryMapper.selectDepartmentJoinMemberList(map);		
+		System.out.println(departmentJoinMemberList + "DepartmentService getDepartmentList departmentJoinMemberList");
+			
+		// 2개 이상의 데이터를 하나로 넘기기 위해 HashMap 사용	
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		returnMap.put("recursiveList", recursiveList);		
+		returnMap.put("departmentJoinMemberList", departmentJoinMemberList);		
+		return returnMap;
 	}	
 	
 	// 카테고리 리스트 조회
