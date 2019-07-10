5cca79a9c6f403a60abcd7cc49cbb0118962cdd9
USER <USER@v161011v>
2019-07-11 00:14:52 +0900

    [2019-07-11 최정환] 관리자 인사 설정 / 학력 정보
    인사설정 화면 수정
    학력정보 수정
    
    <-- View -->
    myEducation.html
    setting.html
    
    <-- Controller -->
    MemberMyEducationRestController.java
    
    <-- domain -->
    MemberEducation.java

--- ksmartware/src/main/java/ksmart31/team01/member/controller/MemberMyEducationRestController.java
+++ ksmartware/src/main/java/ksmart31/team01/member/controller/MemberMyEducationRestController.java
@@ -1,6 +1,8 @@
 package ksmart31.team01.member.controller;
 
 
+import java.io.File;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +20,21 @@ public class MemberMyEducationRestController {
 
 	@Autowired private MemberMyEducationService MemberMyEducationService;
 	
-	@PostMapping("Upload/file")
-	public void UploadFile(MultipartFile file) {
-		System.out.println("파일");
+	/*@PostMapping("Upload/file")
+	public void UploadFile(MemberEducation memberEducation) {
+		System.out.println("입력 실행 ");
+		System.out.println("파일 : " + memberEducation.getMultipartFile());
+		
 		
 		//String path = "D:\\jjdev\\sts_workspace\\mall5.7\\src\\main\\resources\\static\\upload";
-		/*String path = PathStr.UPLOAD_PATH;*/
+		String path = PathStr.UPLOAD_PATH;*/
 		//String path = ServletUriComponentsBuilder.fromCurrentContextPath().path("upload").toUriString();
 		// ServletComponentsBuilder.fromCurrentContextPath	() == request.getContextPath()
 		/*MemberMyEducationService.addBoard(boardRequest, path); 
 		File file = new File("");
-		file.delete();*/
+		file.delete();
 	}
+	*/
 	
 	@GetMapping("/member/memberMyProfile/removeMyEducation")  // 삭제 매서드
 	public void removeMyEducation(String codeNo) {
@@ -41,17 +46,17 @@ public class MemberMyEducationRestController {
 	@GetMapping("/member/memberMyProfile/addmyEducation") // 입력매서드
 	public void addmyEducation(HttpSession session, MemberEducation memberEducation) {
 		System.out.println("모달창 학력정보 입력 매서드 실행 : memberEducation" + memberEducation);
+		
 		if(!memberEducation.getMemberEducationNow().equals("재학")){
-			System.out.println("종료날짜"+memberEducation.getMemberEducationEndDay());
+			System.out.println("종료날짜 : "+memberEducation.getMemberEducationEndDay());
 			memberEducation.setMemberEducationEndDay(null);
-			System.out.println("바뀐 종료날짜"+memberEducation.getMemberEducationEndDay());
+			System.out.println("바뀐 종료날짜 : "+memberEducation.getMemberEducationEndDay());
 		}
-		
-		
 		
 		Member member = (Member) session.getAttribute("loginMember");
 	
-		MemberMyEducationService.addmyEducation(memberEducation, member);	
+		MemberMyEducationService.addmyEducation(memberEducation, member);
+
 	}
 
 }
