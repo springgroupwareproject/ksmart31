ab0571c159995779f53fb301004a44bb1cc936e6
Administrator <Administrator@MSDN-SPECIAL>
2019-07-11 16:28:36 +0900

    [2019-07-11 주진호] 상단 헤더 추가
    - 휴가 종류, 내역 관리 및 나의 휴가 정보 페이지 상단 안내 문구 추가
    
    - 휴가 내역 관리
    	조직도 - 전체 조직원 조회 추가

--- ksmartware/src/main/java/ksmart31/team03/leave/service/LeaveHistoryManagementService.java
+++ ksmartware/src/main/java/ksmart31/team03/leave/service/LeaveHistoryManagementService.java
@@ -130,11 +130,11 @@ public class LeaveHistoryManagementService {
 	public List<LeaveHistory> getleaveHistoryByMemberIdList(String departmentName){	
 		// StringUtils.countMatches() -> departmentName중에 -가 포함되어있는 갯수를 보여준다
 		int departmentNumber = StringUtils.countMatches(departmentName, "-"); 
-		System.out.println("LeaveHistoryManagementService.getDepartmentList departmentNumber -갯수 : " + departmentNumber);
+		System.out.println("LeaveHistoryManagementService.getleaveHistoryByMemberIdList departmentNumber -갯수 : " + departmentNumber);
 		// replaceAll()메서드로 -가 포함된문자열을 없애준다 		
 		if(departmentName != null){	
 			departmentName = departmentName.replaceAll("-","");
-			System.out.println("LeaveHistoryManagementService.getDepartmentList departmentName -제거 확인 : " + departmentName);
+			System.out.println("LeaveHistoryManagementService.getleaveHistoryByMemberIdList departmentName -제거 확인 : " + departmentName);
 		}
 		// 2개 이상의 데이터를 하나로 넘기기 위해 HashMap 사용
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -142,26 +142,26 @@ public class LeaveHistoryManagementService {
 		map.put("departmentNumber", departmentNumber);
 		// 특정 부서에 해당하는  memberId 조회(휴가 내역 출력용)
 		String[] memberId = leaveHistoryMapper.selectDepartmentJoinMemberListForLeaveHistory(map);		
-		System.out.println("LeaveHistoryManagementService.getDepartmentList memberId.length : "+memberId.length);
+		System.out.println("LeaveHistoryManagementService.getleaveHistoryByMemberIdList memberId.length : "+memberId.length);
 		List<LeaveHistory> leaveHistoryByMemberList = new ArrayList<LeaveHistory>();
 		if(memberId.length > 0) {
 			// String 배열 값 확인
 			for (int i=0; i<memberId.length; i++) {
-				System.out.println("LeaveHistoryManagementService.getDepartmentList memberId : "+memberId[i]);
+				System.out.println("LeaveHistoryManagementService.getleaveHistoryByMemberIdList memberId : "+memberId[i]);
 			}
 			// String 배열을 ArrayList에 담고
 			ArrayList<String> memberIdList = new ArrayList<String>();
 			for(String item : memberId) {
 				memberIdList.add(item);
 			}
-			System.out.println("LeaveHistoryManagementService.getDepartmentList memberIdList : "+memberIdList);
+			System.out.println("LeaveHistoryManagementService.getleaveHistoryByMemberIdList memberIdList : "+memberIdList);
 			// ArrayList를 HashMap에 담는다
 			Map<String, Object> memberIdMap = new HashMap<String, Object>();
 			memberIdMap.put("memberIdList", memberIdList);
-			System.out.println("LeaveHistoryManagementService.getDepartmentList memberIdMap : "+memberIdMap.get("memberIdList"));
+			System.out.println("LeaveHistoryManagementService.getleaveHistoryByMemberIdList memberIdMap : "+memberIdMap.get("memberIdList"));
 			// 특정 부서에 해당하는 조직원 휴가 내역 조회
 			leaveHistoryByMemberList = leaveHistoryMapper.selectLeaveHistoryByMemberList(memberIdMap);
-			System.out.println("LeaveHistoryManagementService.getDepartmentList leaveHistoryByMemberList : "+leaveHistoryByMemberList);
+			System.out.println("LeaveHistoryManagementService.getleaveHistoryByMemberIdList leaveHistoryByMemberList : "+leaveHistoryByMemberList);
 		}
 		return leaveHistoryByMemberList;
 	}	
