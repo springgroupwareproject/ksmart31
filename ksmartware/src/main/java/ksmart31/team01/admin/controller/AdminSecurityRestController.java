e360b1ca16bc0ca0e60cc9d06cf09aba55c54748
Administrator <Administrator@MSDN-SPECIAL>
2019-07-15 17:33:01 +0900

    [2019-07-15 최정환] 관리자 권한 부여
     - 수정
     관리자 권한부여 동기방식에서 비동기방식 처리로 변경
    
    [-- View --]
    adminSecurity.html
    AdminSecurityController.java
    AdminSecurityRestController.java

--- ksmartware/src/main/java/ksmart31/team01/admin/controller/AdminSecurityRestController.java
+++ ksmartware/src/main/java/ksmart31/team01/admin/controller/AdminSecurityRestController.java
@@ -1,8 +1,10 @@
 package ksmart31.team01.admin.controller;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ksmart31.team01.admin.service.AdminSecurityService;
@@ -12,18 +14,27 @@ import ksmart31.team01.member.domain.AdminLevel;
 public class AdminSecurityRestController {
 	
 	
-	@Autowired AdminSecurityService personnelSetUpService;
+	@Autowired AdminSecurityService adminSecurityService;
 	
-	/* 관리자 권한부여 저장 매서드 */
-	@GetMapping("/adminLevelSave")
-	public String adminLevel(AdminLevel adminLevel) {
-		System.out.println("관리자별 권한부여 실행");
-		System.out.println(adminLevel);
-		
-		
-		return null;
+	
+	@GetMapping("/admin/adminSecurity/addAdminSecurity")
+	public void addAdminSecurity() {
+		System.out.println("관리자 권한 부여 실행");	
+
 		
 	}
+	
+	/* 관리자 권한부여 출력 매서드 */
+	@GetMapping("/admin/adminSecurity/getAdminSecurity")
+	public List<AdminLevel> adminLevel() {
+		System.out.println("관리자별 권한 현황 출력");
+		
+		List<AdminLevel> adminLevel = adminSecurityService.adminLevel();
+	
+		return adminLevel;
+	}	
+
+		
 
 	
 }
