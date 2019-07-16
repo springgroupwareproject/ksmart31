aaa725c94d8d8ac1a412884d6e279de05b244e0a
Administrator <Administrator@MSDN-SPECIAL>
2019-07-16 17:32:22 +0900

    [2019-07-16 최정환] 관리자 권한 부여
     - 수정
    관리자 보안 부여 저장기능 구현
    
    
    <-- Mapper -->
    AdminSecurityMapper.java 수정
    AdminSecurityMapper.xml 수정
    
    <-- Service -->
    AdminSecurityService.java 수정
    
    <-- Controller -->
    AdminSecurityRestController.java 수정
    
    <-- View -->
    body.html 수정 예정
    adminSecurity.html 자바스크립트 수정기능 구현
    
    <-- xml -->
    pom.xml Gson 라이브러리 추가

--- ksmartware/src/main/java/ksmart31/team01/admin/mapper/AdminSecurityMapper.java
+++ ksmartware/src/main/java/ksmart31/team01/admin/mapper/AdminSecurityMapper.java
@@ -1,12 +1,16 @@
 package ksmart31.team01.admin.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import ksmart31.team01.member.domain.AdminLevel;
 
 @Mapper
 public interface AdminSecurityMapper {
+
+
+	public int updateAdminLevel(Map<String, Object> map); // 관리자 권한부여 수정 매서드
 
 	public List<AdminLevel> adminLevel(); // 보안관리 관리자 권한부여 리스트 출력 매서드
 	
